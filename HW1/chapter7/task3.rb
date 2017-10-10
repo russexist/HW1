0c84e6ca1802f34f6a7aacef21e3bd85a65b4468
Unknown <jambo29@yandex.com>
2017-10-10 22:21:41 +0300

    add hw

--- HW1/chapter7/task3.rb
+++ HW1/chapter7/task3.rb
@@ -1,7 +1,13 @@
-arr = ['Numbers', 'Letters', 'Variables', 'page 1', 'page 72', 'page 118']
+arr = ['Content', 'Chapter 1: Numbers', 'Chapter 2: Letters', 'Chapter 3: Variables', 'page 1', 'page 72', 'page 118']
 lineWidth = 45
+puts arr[0].center(lineWidth)
+puts arr[1].ljust(lineWidth/2) + arr[4].rjust(lineWidth/2)
+puts arr[2].ljust(lineWidth/2) + arr[5].rjust(lineWidth/2)
+puts arr[3].ljust(lineWidth/2) + arr[6].rjust(lineWidth/2)
 
-puts 'Content'.center(lineWidth)
-puts "Chapter 1: #{arr[0]}".ljust(lineWidth/2) + arr[3].rjust(lineWidth/2)
-puts "Chapter 2: #{arr[1]}".ljust(lineWidth/2) + arr[4].rjust(lineWidth/2)
-puts "Chapter 3: #{arr[2]}".ljust(lineWidth/2) + arr[5].rjust(lineWidth/2)
+#or --------------------------------------------
+def output head, content, line_width
+  puts head.center(line_width)
+  content.each { |str| puts str[0].ljust(line_width/2) + str[1].rjust(line_width/2)}
+end
+output('Content', [['Chapter 1: Numbers', 'page 1'], ['Chapter 2: Letters', 'page 72'], ['Chapter 3: Variables', 'page 118']], 45)
---
+++ HW1/chapter8/task4.rb
@@ -0,0 +1,103 @@
+def englishNumber number
+  if number < 0
+    return 'Пожалуйста, введите неотрицательное число.'
+  end
+  if number == 0
+    return 'zero'
+  end
+
+  numString = ''
+
+  onesPlace = ['one',     'two',       'three',    'four',     'five',
+               'six',     'seven',     'eight',    'nine']
+
+  tensPlace = ['ten',     'twenty',    'thirty',   'forty',    'fifty',
+               'sixty',   'seventy',   'eighty',   'ninety']
+  teenagers = ['eleven',  'twelve',    'thirteen', 'fourteen', 'fifteen',
+               'sixteen', 'seventeen', 'eighteen', 'nineteen']
+
+  left = number
+    write = left / 1000000
+  left = left - write * 1000000
+
+  if write > 0
+    million = englishNumber write
+    numString = numString + million + ' million'
+
+    if left > 0
+      numString = numString + ' '
+    end
+  end
+
+
+  write = left / 1000
+  left = left - write * 1000
+
+  if write > 0
+    thousand = englishNumber write
+    numString = numString + thousand + ' thousand'
+
+    if left > 0
+      numString = numString + ' '
+    end
+  end
+
+  write = left/100
+  left  = left - write*100
+
+  if write > 0
+
+    hundred  = englishNumber write
+    numString = numString + hundred + ' hundred'
+
+    if left > 0
+      numString = numString + ' '
+    end
+  end
+
+  write = left/10
+  left  = left - write*10
+
+  if write > 0
+    if ((write == 1) and (left > 0))
+      numString = numString + teenagers[left-1]
+      left = 0
+    else
+      numString = numString + tensPlace[write-1]
+    end
+
+    if left > 0
+      numString = numString + '-'
+    end
+  end
+
+  write = left
+  left  = 0
+
+  if write > 0
+    numString = numString + onesPlace[write-1]
+  end
+
+  numString
+end
+
+amount_bottle = 9999
+amount = amount_bottle
+def bottles_end amount
+  if amount == 1
+    "#{englishNumber amount} bottle"
+  elsif amount > 1
+    "#{englishNumber amount} bottles"
+  else
+    'No more bottles'
+  end
+end
+
+while amount != 0
+  puts "#{bottles_end(amount).capitalize} of beer on the wall, #{bottles_end(amount)} of beer"
+  puts "If one of those bottles should happen to fall, #{bottles_end(amount - 1)} of beer"
+  amount -= 1
+  if amount == 0
+    puts 'And now there are no bottles.'
+  end
+end
