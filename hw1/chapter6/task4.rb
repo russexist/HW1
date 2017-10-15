708bf5b595f41e3f878220e9449cfab0e207436c
Unknown <jambo29@yandex.com>
2017-10-15 23:25:35 +0300

    сorrected errors

--- hw1/chapter6/task4.rb
+++ hw1/chapter6/task4.rb
@@ -4,11 +4,7 @@ puts 'Введте конечный год'
 s_year = gets.chomp.to_i #secondYear
 
 (f_year..s_year).each do |year|
-  if year % 4 == 0 && year % 100 == 0 && year % 400 == 0
-    puts "#{year} - высокосный год"
-  elsif year % 4 == 0 && year % 100 == 0
-    puts "#{year} - HE высокосный год"
-  elsif year % 4  == 0
+  if (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
     puts "#{year} - высокосный год"
   else
     puts "#{year} - HE высокосный год"
